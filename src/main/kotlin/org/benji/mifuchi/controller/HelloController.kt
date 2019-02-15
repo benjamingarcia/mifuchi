a4b3a78e6ae34557e43cda19eee51a71ed6e632a
Benjamin Garcia <bgarcia@tellmeplus.com>
2019-02-15 16:22:01 +0100

    🚧 : add helloworld example

---
+++ src/main/kotlin/org/benji/mifuchi/controller/HelloController.kt
@@ -0,0 +1,16 @@
+package org.benji.mifuchi.controller
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+
+@Controller("/hello")
+class HelloController {
+
+    @Get("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    fun index(): String {
+        return "Hello World"
+    }
+}
